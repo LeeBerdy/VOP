9b42c84e2875848971bf105bfdab0b144d767597
LeeBerdy <145404033+LeeBerdy@users.noreply.github.com>
2024-02-20 19:29:08 +0100

    update

---
+++ JS/Parallax - prices.js
@@ -0,0 +1,11 @@
+
+$(window).scroll(function() {
+    parallax();
+  })
+
+function parallax() {
+
+    var wScroll = $(window).scrollTop()-1100;
+    
+    $('.parallax--bg').css('background-position', 'center '+(wScroll*0.2)+'px')
+}
