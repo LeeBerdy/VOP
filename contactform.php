5208fb4431249b903fe165668ef5d7649350713a
LeeBerdy <145404033+LeeBerdy@users.noreply.github.com>
2024-03-02 15:13:04 +0100

    php

---
+++ contactform.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+
+    $mailTo = "pospishil@vop-develop.cz";
+    $headers = "From: ".$mailFrom;
+    $txt = "Přišel ti email od ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.html")
+}
