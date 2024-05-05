a3e9694491dd8ad43a5b86997644248a09718f8d
LeeBerdy <145404033+LeeBerdy@users.noreply.github.com>
2024-05-05 23:48:31 +0200

    delete

--- JS/AC.js
+++
@@ -1,8 +0,0 @@
-const AC_btn = document.querySelector('.AC');
-const tabulka = document.querySelector('.Tabulka');
-
-AC_btn.addEventListener('click', function () {
-    AC_btn.classList.toggle('changed');
-    tabulka.classList.toggle('changed');
-
-});
